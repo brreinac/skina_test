f7af82db058d86298dea55c22c6a78c593f84cf9
brreinac <60161411+brreinac@users.noreply.github.com>
2025-08-07 13:55:55 -0500

    feat: make  migrations

---
+++ database/migrations/2025_08_07_184027_create_subcategories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->boolean('is_active')->default(true);
+            $table->unsignedInteger('products_count')->default(0);
+            $table->timestamps();
+
+            $table->unique(['category_id','name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subcategories');
+    }
+};
